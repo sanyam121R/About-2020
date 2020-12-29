623490f3fa45d60d0f1d94bb4ca54320bfd18787
sanyam121R <sanyam.12rathore@gmail.com>
2020-12-30 00:37:03 +0530

    added menu button

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlide = () => {
+
+    const Menu = document.querySelector('.wrapper-menu');
+    const nav = document.querySelector('.sidebar');
+    const navLinks = document.querySelectorAll('.sidebar-list li');
+
+    Menu.addEventListener('click', () => {
+        Menu.classList.toggle('open');
+        nav.classList.toggle('sidebar-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/11 + 0.15}s`;
+            }
+        });
+    })
+}
+
+
+const app = () => {
+    navSlide();
+}
+
+app();
