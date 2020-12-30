8eeb16e2075f6bc799d8cae264fa3d4b342ed59d
sanyam121R <sanyam.12rathore@gmail.com>
2020-12-30 21:26:52 +0530

    changed opacity on li tag

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const navSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/11 + 0.15}s`;
+                link.style.animation = `navLinkFade 0.5s ease-in forwards ${index/11 + 0.15}s`;
             }
         });
     })
