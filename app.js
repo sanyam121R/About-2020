47cf23a1c3fcb6cd85c467c5102847a4198e6440
sanyam121R <sanyam.12rathore@gmail.com>
2020-12-31 15:09:28 +0530

    removed bug from scroll to top button

--- app.js
+++ app.js
@@ -3,19 +3,35 @@ const navSlide = () => {
     const Menu = document.querySelector('.wrapper-menu');
     const nav = document.querySelector('.sidebar');
     const navLinks = document.querySelectorAll('.sidebar-list li');
+    const buttonforTop = document.querySelector('.moveToTop');
+
+    buttonforTop.style.visibility = "hidden";
 
     Menu.addEventListener('click', () => {
         Menu.classList.toggle('open');
         nav.classList.toggle('sidebar-active');
+        buttonforTop.style.visibility = "hidden";
 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
+                buttonforTop.style.visibility = "visible";
             } else {
                 link.style.animation = `navLinkFade 0.5s ease-in forwards ${index/11 + 0.15}s`;
             }
         });
     })
+    
+    window.onscroll = () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            buttonforTop.style.visibility = "visible";
+            buttonforTop.style.animation = `fadeIn 1s`;
+        } else {
+            buttonforTop.style.animation = '';
+            buttonforTop.style.visibility = "hidden";
+
+          }
+    };
 }
 
 
